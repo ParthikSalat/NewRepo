ce5167b2c451827574e96004b9799d5bfbcaeb05
parthik <parthik@DESKTOP-5F1S63A>
2024-10-17 16:37:11 +0530

    Event contrroler

---
+++ Frontend/Controllers/EventController.cs
@@ -0,0 +1,97 @@
+﻿using EventAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Frontend.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: EventController
+        string apiUrl = "https://localhost:7121/api/EventTbs";
+        HttpClient client=new HttpClient();
+        public async Task<ActionResult> Index()
+        {
+            var a = await client.GetFromJsonAsync<List<EventTb>>($"{apiUrl}");
+            return View(a);
+        }
+
+        // GET: EventController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EventController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(EventTb data)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await client.PostAsJsonAsync($"{apiUrl}", data);
+                    Console.WriteLine(data);
+                    return RedirectToAction(nameof(Index));
+
+                }
+                else { return View(); }
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EventController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EventController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
