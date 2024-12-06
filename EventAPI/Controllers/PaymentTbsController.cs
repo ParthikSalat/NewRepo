53e6c77b4fd9132398f241bb6758042319603442
parthik <parthik@DESKTOP-5F1S63A>
2024-12-06 14:59:50 +0530

    ticket controller

---
+++ EventAPI/Controllers/PaymentTbsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EventAPI.Models;
+
+namespace EventAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentTbsController : ControllerBase
+    {
+        private readonly EventDbContext _context;
+
+        public PaymentTbsController(EventDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentTbs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentTb>>> GetPaymentTbs()
+        {
+            return await _context.PaymentTbs.ToListAsync();
+        }
+
+        // GET: api/PaymentTbs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentTb>> GetPaymentTb(int id)
+        {
+            var paymentTb = await _context.PaymentTbs.FindAsync(id);
+
+            if (paymentTb == null)
+            {
+                return NotFound();
+            }
+
+            return paymentTb;
+        }
+
+        // PUT: api/PaymentTbs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentTb(int id, PaymentTb paymentTb)
+        {
+            if (id != paymentTb.PaymentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymentTb).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentTbExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PaymentTbs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PaymentTb>> PostPaymentTb(PaymentTb paymentTb)
+        {
+            _context.PaymentTbs.Add(paymentTb);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPaymentTb", new { id = paymentTb.PaymentId }, paymentTb);
+        }
+
+        // DELETE: api/PaymentTbs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentTb(int id)
+        {
+            var paymentTb = await _context.PaymentTbs.FindAsync(id);
+            if (paymentTb == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentTbs.Remove(paymentTb);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PaymentTbExists(int id)
+        {
+            return _context.PaymentTbs.Any(e => e.PaymentId == id);
+        }
+    }
+}
---
+++ EventAPI/Controllers/TicketTbsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EventAPI.Models;
+
+namespace EventAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketTbsController : ControllerBase
+    {
+        private readonly EventDbContext _context;
+
+        public TicketTbsController(EventDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TicketTbs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TicketTb>>> GetTicketTbs()
+        {
+            return await _context.TicketTbs.ToListAsync();
+        }
+
+        // GET: api/TicketTbs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TicketTb>> GetTicketTb(int id)
+        {
+            var ticketTb = await _context.TicketTbs.FindAsync(id);
+
+            if (ticketTb == null)
+            {
+                return NotFound();
+            }
+
+            return ticketTb;
+        }
+
+        // PUT: api/TicketTbs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTicketTb(int id, TicketTb ticketTb)
+        {
+            if (id != ticketTb.TicketId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ticketTb).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TicketTbExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TicketTbs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TicketTb>> PostTicketTb(TicketTb ticketTb)
+        {
+            _context.TicketTbs.Add(ticketTb);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TicketTbExists(ticketTb.TicketId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTicketTb", new { id = ticketTb.TicketId }, ticketTb);
+        }
+
+        // DELETE: api/TicketTbs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTicketTb(int id)
+        {
+            var ticketTb = await _context.TicketTbs.FindAsync(id);
+            if (ticketTb == null)
+            {
+                return NotFound();
+            }
+
+            _context.TicketTbs.Remove(ticketTb);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TicketTbExists(int id)
+        {
+            return _context.TicketTbs.Any(e => e.TicketId == id);
+        }
+    }
+}
